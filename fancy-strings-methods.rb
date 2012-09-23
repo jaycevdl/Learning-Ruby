b8c60b7f81c31102daff6b9f2a1696c21ddbb54a
jaycevdl <jayce@vanstudiophotography.com>
2012-09-23 00:44:54 -0600

    Fancy String Methods Chapter
    The fancy string method chapter in "Learn to Program" by Chris

---
+++ fancy-strings-methods.rb
@@ -0,0 +1,22 @@
+puts var1 = 'stop'
+puts var2 = 'deliver repaid desserts'
+puts var3 = '....TCELES B HSUP A magic spell?'
+
+puts'Normal'
+puts''
+puts var1 
+puts var2 
+puts var3 
+puts''
+puts'Reverse'
+puts''
+puts var1.reverse
+puts var2.reverse 
+puts var3.reverse
+puts''
+puts 'Length'
+puts''
+puts' what\'s your full name?'
+name= gets.chomp!
+puts' did you know there are ' + name.length.to_s + ' characters'
+puts ' in your name?'
