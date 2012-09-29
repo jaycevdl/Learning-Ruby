ef3b81892b160159a2a6eba2f1849fb74d102b21
jaycevdl <jayce@vanstudiophotography.com>
2012-09-28 21:12:03 -0600

    Dealing with Letter Case

---
+++ Angry-Boss.rb
@@ -0,0 +1,12 @@
+# Angry Boss, The app that yell back!
+
+puts""
+puts"You just walked into your bosses office."
+puts""
+puts"(now ask him a question)"
+question= gets.chomp!
+puts""
+boss1= "What do you mean "
+boss2= " you're fired!!??!!"
+puts boss1.upcase+"'"+ question.upcase+ "!'"+ boss2.upcase
+puts""
